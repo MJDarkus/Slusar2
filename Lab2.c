4b47b11e580325508bab1443b4acdf4d07714315
MJDarkus <49124489+MJDarkus@users.noreply.github.com>
2022-06-12 21:30:17 +0300

    Update Lab2.c

--- Lab2.c
+++ Lab2.c
@@ -4,11 +4,11 @@
 #PBS -l ncpus=2
 #PBS -N Marchenko_lab2
 #PBS -t 1-100
-#PBS –W stagein=/mnt/work/tb434/mpi@plus7.cluster.univ.kiev.ua:/home/grid/testbed/tb434/5kurs/lab2/mpt
+#PBS –W stagein=/mnt/work/tb382/mpi@plus7.cluster.univ.kiev.ua:/home/grid/testbed/tb382/Slusar2/lab2/mpt
 
-#PBS -W stageout=/mnt/work/tb434/output.task*@plus7.cluster.univ.kiev.ua:/home/grid/testbed/tb434/5kurs/lab2/final
+#PBS -W stageout=/mnt/work/tb382/output.task*@plus7.cluster.univ.kiev.ua:/home/grid/testbed/tb382/Slusar2/lab2/final
 
-cd ~/5kurs/lab2/final
+//cd ~/5kurs/lab2/final
 
 c=9
 ml icc
@@ -35,12 +35,12 @@ elif [ $(($PBS_ARRAYID%10)) -eq 0 ]; then
   else 
     c=9; 
   fi
-  while [ $(ls $(seq -f ‘output.task%g' $(($PBS_ARRAYID-$c)) $(($PBS_ARRAYID-1))) | wc -l) -ne $c ]; do
+  while [ $(ls $(seq -f 'output.task%g' $(($PBS_ARRAYID-$c)) $(($PBS_ARRAYID-1))) | wc -l) -ne $c ]; do
     sleep 3
   done 
-  cat $(seq -f ‘output.task%g' $(($PBS_ARRAYID-$c)) $(($PBS_ARRAYID-1))) |\
+  cat $(seq -f 'output.task%g' $(($PBS_ARRAYID-$c)) $(($PBS_ARRAYID-1))) |\
   awk '{ split($0,a,""); split(a[5],b,","); print b[1]; }' |\
   uniq -c > results.task$PBS_ARRAYID
 else
-  /mnt/work/tb434/mpi > /mnt/work/tb434/output.task$PBS_ARRAYID
+  /mnt/work/tb382/mpi > /mnt/work/tb382/output.task$PBS_ARRAYID
 fi
